4ef87c51939eee5a6ef6b0ee8714e9252f1e193e
bellatrick <busayosamuel2016@gmail.com>
2024-08-16 23:38:09 +0100

    added all files for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+import Navigation from './components/Navigation';
+import Section from './components/Section';
+// ABIs
+import Ethcommerce from './abis/Ethcommerce.json';
+
+// Config
+import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [ethcommerce, setEthcommerce] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [item, setItem] = useState({});
+
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
+  };
+  const loadBlockchainData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const loadedEthcommerce = new ethers.Contract(
+      config[network.chainId].ethcommerce.address,
+      Ethcommerce,
+      provider
+    );
+    setEthcommerce(loadedEthcommerce);
+
+    const items = [];
+
+    if (ethcommerce) {
+      for (var i = 0; i < 9; i++) {
+        const item = await ethcommerce.items(i + 1);
+        items.push(item);
+      }
+    }
+    const electronics = items.filter((item) => item.category === 'electronics');
+    const clothing = items.filter((item) => item.category === 'clothing');
+    const toys = items.filter((item) => item.category === 'toys');
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Dappazon Best Sellers</h2>
+      {electronics && clothing && toys && (
+        <>
+          <Section
+            title={'Clothing & Jewelry'}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={'Electronics & Gadgets'}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={'Toys & Gaming'} items={toys} togglePop={togglePop} />
+
+        </>
+      )}
+            {toggle && (
+        <Product item={item} provider={provider} account={account} dappazon={dappazon} togglePop={togglePop} />
+      )}
     </div>
   );
 }
